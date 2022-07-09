37cc5f5b4931f0414e971c09d8da7a97949d0bab
NonbinaryCoder <nonbinarycoderdeveloping@gmail.com>
2022-07-09 10:29:47 -0400

    Now enables ansi on Windows Added dependency to `enable-ansi-support` Version 1.0.0

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ mod get_entries;
 mod parse_args;
 
 fn main() {
+    if let Err(e) = enable_ansi_support::enable_ansi_support() {
+        println!("Unable to enable ansi support: {}", e);
+    }
     match parse_args() {
         Some(settings) => {
             if settings.explore {
